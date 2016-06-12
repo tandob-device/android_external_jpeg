ad1b3f57d3b5bbf3fb02b129f7f6f5dc4c9bccfd
Wang Xuerui <idontknw.wang@gmail.com>
2016-06-13 00:46:23 +0800

    workaround Meizu legacy camera blob crash
    Change-Id: I4732fa2c7652309c83487f66b5e390ec9f32af9a

--- jcapimin.c
+++ jcapimin.c
@@ -35,9 +35,11 @@ jpeg_CreateCompress (j_compress_ptr cinfo, int version, size_t structsize)
   cinfo->mem = NULL;		/* so jpeg_destroy knows mem mgr not called */
   if (version != JPEG_LIB_VERSION)
     ERREXIT2(cinfo, JERR_BAD_LIB_VERSION, JPEG_LIB_VERSION, version);
+#ifndef USE_LEGACY_MTK_AV_BLOB
   if (structsize != SIZEOF(struct jpeg_compress_struct))
     ERREXIT2(cinfo, JERR_BAD_STRUCT_SIZE, 
 	     (int) SIZEOF(struct jpeg_compress_struct), (int) structsize);
+#endif
 
   /* For debugging purposes, we zero the whole master structure.
    * But the application has already set the err pointer, and may have set
